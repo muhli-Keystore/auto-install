78a73b2eed46e3aec42cfb77f26d45ea8d9106e3
@muhi.keystore <122099641+muhli-Keystore@users.noreply.github.com>
2023-01-10 11:29:16 +0700

    Add files via upload

--- key.sh
+++ key.sh
@@ -1,1 +1,28 @@
+echo
+echo "\033[1;37m sedang membuka file scrift"
+sleep 10
 clear
+pkg install figlet
+echo "\033[1;37m"
+figlet "base muhli"
+echo
+echo " apabila ada update di dalam scrift"
+echo " harap riset dulu scrift nya dulu"
+echo " Y=iya | T=tidak atau lewat saja"
+echo
+read -p " riset scrift pilih Y/T" adb
+if [ $adb ="Y" ];
+then
+ls
+rm -rf 000
+ls && rm -rf 000
+pkg install git
+git clone https://github.com/muhli-Keystore/000
+cd 000
+sh auto-cmd.sh
+else
+if [ $adb ="T" ];
+then
+cd 000
+sh auto-cmd.sh
+fi fi
