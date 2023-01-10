14e4133835079bf21514cca537af3e351c4ec6ff
@muhi.keystore <122099641+muhli-Keystore@users.noreply.github.com>
2023-01-10 12:01:17 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# key
+## info
+```
+script ini khusus tools AUTO CMD
+```
+## cara install keystore
+```bash
+$ pkg update && pkg upgrade
+$ pkg install git
+$ git clone https://github.com/muhli-Keystore/key
+$ cd key
+$ sh key.sh
--- key.sh
+++ key.sh
@@ -1,28 +1,34 @@
 echo
-echo "\033[1;37m sedang membuka file scrift"
 sleep 10
 clear
 pkg install figlet
 echo "\033[1;37m"
 figlet "base muhli"
 echo
-echo " apabila ada update di dalam scrift"
-echo " harap riset dulu scrift nya dulu"
-echo " Y=iya | T=tidak atau lewat saja"
+echo " apabila ada update di dalam tools"
+echo " harap riset dulu script nya dulu"
+echo " Y= iya" 
+echo " T= lewat saja"
 echo
-read -p " riset scrift pilih Y/T" adb
-if [ $adb ="Y" ];
+read -p " riset script pilih Y/T : " adb
+if [ $adb = "Y" ];
 then
-ls
 rm -rf 000
 ls && rm -rf 000
 pkg install git
 git clone https://github.com/muhli-Keystore/000
 cd 000
 sh auto-cmd.sh
+echo "\033[1;31m *** >> sedang membuat tools << ***
+echo "\033[1;37m"
 else
-if [ $adb ="T" ];
+if [ $adb = "T" ];
 then
 cd 000
 sh auto-cmd.sh
+else
+echo
+echo " kode yang di masukkan salah Y/T"
+echo " pake huruf besar"
+sh key sh
 fi fi
