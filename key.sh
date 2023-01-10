271d253bf3ad988252dd66a31a509c276c8b051a
@muhi.keystore <122099641+muhli-Keystore@users.noreply.github.com>
2023-01-10 14:39:02 +0700

    Add files via upload

--- key.sh
+++ key.sh
@@ -5,12 +5,23 @@ pkg install figlet
 echo "\033[1;37m"
 figlet "base muhli"
 echo
-echo " apabila ada update di dalam tools"
-echo " harap riset dulu script nya dulu"
+sleep 0
+echo " \033[1;33mbuka link di bawah dengan google"
+sleep 0
+echo " atau chrome untuk cek apakah ada update atau tidak"
+sleep 0
+echo "\033[1;36m https://muhli.000webhostapp.com"
+echo 
+echo
+echo "\033[1;37m"
+echo " apabila ada update di dalam tools mohon"
+echo " riset dulu tenang data yang udah di"
+echo " download tidak akan hilang dan tetap"
+echo " masih ada di penyimpanan"
 echo " Y= iya" 
 echo " T= lewat saja"
 echo
-read -p " riset script pilih Y/T : " adb
+read -p " riset tools AUTO CMD pilih Y/T : " adb
 if [ $adb = "Y" ];
 then
 rm -rf 000
