510379b76b7c7ba8c6d7caa45e18967d3e4d8d9f
@muhi.keystore <122099641+muhli-Keystore@users.noreply.github.com>
2023-02-10 21:34:00 +0700

    Add files via upload

--- key.sh
+++ key.sh
@@ -6,16 +6,6 @@ apt install figlet
 echo "\033[1;37m"
 figlet "base muhli"
 echo
-sleep 0
-echo " \033[1;33mbuka link di bawah dengan google"
-sleep 0
-echo " atau chrome untuk masuk ke website resmi"
-sleep 0
-echo " dan lihat tutorial cara install script nya juga"
-sleep 0
-echo "\033[1;36m https://muhli.000webhostapp.com"
-echo 
-echo
 echo "\033[1;37m"
 sleep 0
 echo " apabila ada update di dalam tools mohon"
@@ -26,7 +16,7 @@ echo " download tidak akan hilang dan tetap"
 sleep 0
 echo " masih ada di penyimpanan"
 sleep 0
-echo " [ Y ] iya" 
+echo " [ Y ] riset tools" 
 sleep 0
 echo " [ T ] lewat saja"
 echo
@@ -42,7 +32,7 @@ ls && rm -rf 000
 apt install git
 git clone https://github.com/muhli-Keystore/000
 cd 000
-echo "\033[1;31m *** >> sedang membuat tools << ***"
+echo "\033[1;31m *** >> sedang membuka tools << ***"
 echo "\033[1;37m"
 sleep 5
 sh auto-cmd.sh
@@ -52,7 +42,7 @@ then
 apt install git
 git clone https://github.com/muhli-Keystore/000
 cd 000
-echo "\033[1;31m *** >> sedang membuat tools << ***"
+echo "\033[1;31m *** >> sedang membuka tools << ***"
 echo "\033[1;37m"
 sleep 5
 sh auto-cmd.sh
